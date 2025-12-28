5b026520e35079e8b6a3766c5d60aaa538ceac73
Robin Drexler <drexler.robin@gmail.com>
2025-12-27 19:41:50 -0500

    add tailwind;

--- src/web/client/vite.config.js
+++ src/web/client/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => ({
-  plugins: [preact()],
+  plugins: [tailwindcss(), preact()],
   build: {
     outDir: "../dist",
     emptyOutDir: true,
